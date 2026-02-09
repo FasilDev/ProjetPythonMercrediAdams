5bff49fd02e293511dfd53becd62820c9acf80e4
patinlucas095-rgb <patinlucas095@gmail.com>
2026-02-09 08:16:56 +0100

    update
    Removed comments from the piece class to clean up the code.

--- piece.py
+++ piece.py
@@ -11,12 +11,10 @@ class Piece(pygame.sprite.Sprite):
         self.taille = 24
         self.speed = vitesse
 
-        # Animation de la piece
         self.frame = 0
         self.timer = 0
         self.vitesse_anim = 5
 
-        # Creer la surface
         self.image = pygame.Surface((self.taille, self.taille), pygame.SRCALPHA)
         self._dessiner_piece()
 
@@ -38,7 +36,6 @@ class Piece(pygame.sprite.Sprite):
     def _dessiner_piece(self):
         self.image.fill((0, 0, 0, 0))
 
-        # Couleurs de la piece
         centre = self.taille // 2
 
         pulse = abs(self.frame - 2)
@@ -51,7 +48,7 @@ class Piece(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, (255, 230, 150), (centre - 2, centre - 2), rayon // 3)
 
     def update(self):
-        # Les pieces collectees reapparaissent apres un delai
+        
         if self.collectee:
             self.timer += 1
             if self.timer >= 120: 
@@ -60,20 +57,18 @@ class Piece(pygame.sprite.Sprite):
 
         self.rect.x -= self.speed
 
-        # Animation
         self.timer += 1
         if self.timer >= self.vitesse_anim:
             self.timer = 0
             self.frame = (self.frame + 1) % 4
             self._dessiner_piece()
 
-        # Reinitialiser quand elle sort de l'ecran
         if self.rect.right < 0:
             self.reinitialiser()
 
     def reinitialiser(self):
         self.rect.x = WIDTH + random.randint(100, 400)
-        # Hauteurs variees 
+
         hauteurs = [HEIGHT - 140, HEIGHT - 180, HEIGHT - 220, HEIGHT - 280]
         self.rect.y = random.choice(hauteurs)
         self.collectee = False
